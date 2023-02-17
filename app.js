35c77888c4caeac4b229dc3bb51ef72dc6943017
alibarmonster <alibar838@gmail.com>
2023-02-18 03:50:49 +0700

    add dev and prod mode

--- app.js
+++ app.js
@@ -8,5 +8,5 @@ dotenv.config({ path: './config/config.env' });
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-  console.log(`Server listen on port ${process.env.PORT}`);
+  console.log(`Server listen on port ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
 });
