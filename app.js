c0bb7f85b3ec1d90744a7698e979acce0d7d1714
alibarmonster <alibar838@gmail.com>
2023-02-18 03:44:30 +0700

    fixing bug config

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express();
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config/config.env' });
+
 const PORT = process.env.PORT;
+
 app.listen(PORT, () => {
-  console.log('Server listn on port ${process.env.PORT}');
+  console.log(`Server listen on port ${process.env.PORT}`);
 });
