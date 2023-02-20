5f41e00f81ca405df9692c94fb3466e9be4dbc8b
alibarmonster <alibar838@gmail.com>
2023-02-20 14:45:10 +0700

    Get Jobs Statistics using Aggregation

--- routes/jobs.js
+++ routes/jobs.js
@@ -7,11 +7,13 @@ const {
   updateJob,
   deleteJob,
   getJob,
+  jobStats,
 } = require('../controllers/jobsController');
 
 router.route('/jobs').get(getJobs);
 router.route('/jobs/:id/:slug').get(getJob);
 router.route('/jobs/:zipcode/:distance').get(getJobsInRadius);
+router.route('/stats/:topic').get(jobStats);
 
 router.route('/jobs/new').post(newJob);
 
