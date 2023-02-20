26cad633b6606a61095e6d065c268f138640b1a3
alibarmonster <alibar838@gmail.com>
2023-02-20 12:55:01 +0700

    update job

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getJobs, newJob, getJobsInRadius } = require('../controllers/jobsController');
+const { getJobs, newJob, getJobsInRadius, updateJob } = require('../controllers/jobsController');
 
 router.route('/jobs').get(getJobs);
 router.route('/jobs/:zipcode/:distance').get(getJobsInRadius);
+
 router.route('/jobs/new').post(newJob);
+
+router.route('/jobs/:id').put(updateJob);
 module.exports = router;
