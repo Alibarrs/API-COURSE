13abd6c2457132d4cbaaae95fce73b82a8406162
alibarmonster <alibar838@gmail.com>
2023-02-20 13:04:12 +0700

    delete job

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { getJobs, newJob, getJobsInRadius, updateJob } = require('../controllers/jobsController');
+const {
+  getJobs,
+  newJob,
+  getJobsInRadius,
+  updateJob,
+  deleteJob,
+} = require('../controllers/jobsController');
 
 router.route('/jobs').get(getJobs);
 router.route('/jobs/:zipcode/:distance').get(getJobsInRadius);
 
 router.route('/jobs/new').post(newJob);
 
-router.route('/jobs/:id').put(updateJob);
+router.route('/jobs/:id').put(updateJob).delete(deleteJob);
 module.exports = router;
